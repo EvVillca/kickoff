22add42b22643c81753eef9acb44302ccc97e39d
Evert Villca Salguero <evillca-@c2r19s6.42madrid.com>
2024-10-03 21:34:43 +0200

    a

--- ft_split.c
+++ ft_split.c
@@ -11,9 +11,50 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h> // ELIMINAR
 
-/**/
+/*Contamos el número de PLBs, para el tam del array*/
+static int	count_words(char const *s, char c)
+{
+	int	i;
+	int	words;
+	int	control;
+
+	i = 0;
+	words = 0;
+	control = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			control = 0;
+		}
+		else if (control == 0)
+		{
+			control++;
+			words++;
+		}
+		i++;
+	}
+	return (words);
+}
+
+static void	aux_split(char const *s, char c, char **strings)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+}
+
+/*Devuelve un array de strings, separado por el delimitador*/
 char	**ft_split(char const *s, char c)
 {
+	char	**strings;
 
+	strings = (char **) malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (strings == NULL)
+		return (NULL);
+	aux_split(s, c, strings);
+	return (strings);
 }
--- libft.h
+++ libft.h
@@ -34,11 +34,11 @@ int 	ft_tolower(int c);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 
+char	**ft_split(char const *s, char c);
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-/*char	**ft_split(char const *s, char c);*/
 
 #endif
